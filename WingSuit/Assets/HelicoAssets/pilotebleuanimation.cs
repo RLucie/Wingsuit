defa52429c2cbb89669b7c56225b81692385d727
Alexandre-Mar <63320285+Alexandre-Mar@users.noreply.github.com>
2021-01-12 20:56:02 +0100

    alextrois
    alextrois

---
+++ WingSuit/Assets/HelicoAssets/pilotebleuanimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pilotebleuanimation : MonoBehaviour
+{
+    public AnimationClip Scene1;
+    Animation source;
+    public bool alreadyPlayed = false;
+
+    void Start()
+    {
+        source = GetComponent<Animation>();
+    }
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            source.Play();
+            alreadyPlayed = true;
+        }
+    }
+}
